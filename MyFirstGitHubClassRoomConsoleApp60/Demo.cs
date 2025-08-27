9ec533a47a709320726d16f491660be4499b60d5
Kia Thorsen <kiax0758@edu.zealand.dk>
2025-08-27 11:02:29 +0200

    Demo klasse lavet

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        public string Navn { get; set; }
+        public int Id { get; set; }
+    }
+}
